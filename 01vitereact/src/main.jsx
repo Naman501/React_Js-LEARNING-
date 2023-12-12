3aeaede29b4c972520ecb18523ef3748b92c3687
Naman Mittal <namanmittal501@gmail.com>
2023-12-12 20:11:35 +0530

    Custom_React

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+// function MyApp ( ){
+// return(
+//   <div>
+//     <h1>My App</h1>
+//   </div>
+// )
+// }
+
+const anotherElement= ( 
+  <a href="https://google.com">Visit Google</a>
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+anotherElement 
 )
