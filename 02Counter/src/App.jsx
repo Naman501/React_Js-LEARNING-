d84378945e25973d7abfdfaeb34d5cc0d8c8e5ba
Naman Mittal <namanmittal501@gmail.com>
2023-12-13 12:50:10 +0530

    React_HOOKS

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(6)
+
+
+
+  // const [count, setCount] = useState(0)
+  // let counter=6
+  const addValue=()=>{
+    console.log("cicked",counter);
+    // counter=counter+1
+    // console.log("value Added :",Math.random())
+    setCounter(counter+1)
+   
+  
+    }
+
+  
+
+  const removeValue=()=>{
+    console.log("clicked",counter);
+    setCounter(counter-1)
+  }
+
+  return (
+    <>
+      <h1>NAMAN MITTAL</h1>
+      <h2>Counter Value :{counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add Value {counter}</button>
+      <br />
+      <br />
+        <button
+        onClick={removeValue}>Remove Value {counter}</button>
+        <br />
+        <p>Footer {counter}</p>
+      
+  </>
+  )
+}
+
+export default App
