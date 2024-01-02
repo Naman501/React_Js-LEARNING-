6806fd76e11dea1d0953bb6ba85a61623bde8e04
Naman Mittal <namanmittal501@gmail.com>
2024-01-02 17:04:56 +0530

    Currency_Convertor

---
+++ 06_Currency_Convertor/src/hooks/useCurrencyinfo.js
@@ -0,0 +1,21 @@
+// function hello(){
+//     return "Hello World";
+// }
+
+// hello();
+import { useEffect,useState } from "react";
+const [data,setData] = useState({})
+function useCurrencyinfo(currency){
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res)=>res.json())
+        .then((res)=>setData(res[currency]))
+        console.log(data);
+    }
+    ,[currency])
+    console.log(data);
+    return data;
+}
+
+
+export default useCurrencyinfo;
