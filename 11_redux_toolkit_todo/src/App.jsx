1aae5b19b50f1db7d1aed96e054b9c0428acff70
Naman Mittal <namanmittal501@gmail.com>
2024-03-24 01:17:10 +0530

    Redux Toolkit

---
+++ 11_redux_toolkit_todo/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+
+import './App.css'
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todos'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-6xl text-blue-300'>NIGGA</h1>
+    <AddTodo/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
